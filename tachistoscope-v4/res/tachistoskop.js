cf25f8eef48dc54a9853d425386b95b75d501210
j0000r111 <janhenning.raff@gmail.com>
2021-05-25 22:11:21 +0200

    git it!

---
+++ tachistoscope-v4/res/tachistoskop.js
@@ -0,0 +1,40 @@
+// timer
+var timer1, timer2, timer3;
+var shutterspeed = 25;
+
+$(".va-button-show-image").on("click", function() {
+  shutterspeed = $(".va-input-shutterspeed").val();
+
+  $(".va-img-holder__shutter").css("background-image", "none");
+  timer1 = setTimeout(function() {
+    $(".va-img-holder__shutter").hide();
+    timer2 = setTimeout(function() {
+      $(".va-img-holder__shutter").show();
+      timer3 = setTimeout(function() {
+        $(".va-img-holder__shutter").css("backgroundImage", "url(res/kreis400px.png)");
+        clearTimeout(timer1);
+        clearTimeout(timer2);
+        clearTimeout(timer3);
+      }, 2000);
+    }, shutterspeed);
+  }, 4000);
+});
+
+$(".va-button-reveal-image").on("click", function() {
+  $(".va-img-holder__shutter").toggle();
+});
+
+// file input
+function readURL(input) {
+  if (input.files && input.files[0]) {
+    var reader = new FileReader();
+    reader.onload = function(e) {
+      $(".va-img-holder__image").attr("src", e.target.result);
+    };
+    reader.readAsDataURL(input.files[0]);
+  }
+}
+
+$(".va-fileinput").change(function() {
+  readURL(this);
+});
